595eb2894bb5c207b78943a7b997a3ffae765f83
ap1w1 <96704254+ap1w1@users.noreply.github.com>
2026-08-17 04:33:12 +0300

    Update psiphon_install.sh

--- psiphon_install.sh
+++ psiphon_install.sh
@@ -27,7 +27,7 @@ for command in docker systemctl curl ss nft; do
   command -v "$command" >/dev/null || die "$command is required"
 done
 
-IMAGE=swarupsengupta2007/psiphon:latest
+IMAGE=ghcr.io/charafreedom/psiphon:latest
 SOCKS_PORT=1080
 HTTP_PORT=8080
 DEVICE_REGION=
